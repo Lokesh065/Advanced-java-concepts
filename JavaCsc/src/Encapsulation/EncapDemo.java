3ec5feb08fb711adb4d057ce013ba02e9fb4c2c2
lokesh <lokesh@LAPTOP-B6SAJ78U>
2024-06-01 08:47:51 +0530

    Encapsulation

---
+++ JavaCsc/src/Encapsulation/EncapDemo.java
@@ -0,0 +1,18 @@
+package Encapsulation;
+
+public class EncapDemo {
+
+	public static void main(String[] args) {
+		    
+
+		        OopsConceptDemo obj = new OopsConceptDemo();
+		        obj.setSerialNum(001);
+		        obj.setName("Pooja");
+		        obj.setAge(20);
+		        System.out.println(obj);
+		    }
+
+
+	}
+
+
